52bd2756b5af5dfbeed8ae5e31a9a56157beec7c
sihlp <sihlp@vng.com.vn>
2021-05-02 02:11:44 +0700

    change brew package

---
+++ setup/git/github_token.zsh
@@ -0,0 +1,4 @@
+# Set Github tokens for Github Dashboard
+
+export GITHUB_TOKEN="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"
+export JEKYLL_GITHUB_TOKEN="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"
--- setup/homebrew/casks.sh
+++ setup/homebrew/casks.sh
@@ -9,16 +9,16 @@ brew install 'sourcetree'
 # productivity
 brew install 'alfred'
 brew install 'flux'
-brew install 'popclip'
+#brew install 'popclip'
 brew install 'hammerspoon'
 brew install 'karabiner-elements'
-brew install '1password'
+#brew install '1password'
 brew install 'simplenote'
 brew install 'the-unarchiver'
 
 # basics
-brew install 'dropbox'
-brew install 'spotify'
+#brew install 'dropbox'
+#brew install 'spotify'
 
 # fonts
 brew install 'font-jetbrains-mono'
