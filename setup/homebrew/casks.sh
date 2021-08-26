7c57f7e387fcc0c3bf4936232457dd2fc47c767d
sihlp <sihlp@vng.com.vn>
2021-08-26 16:33:25 +0700

    replace alfred with raycast

--- setup/homebrew/casks.sh
+++ setup/homebrew/casks.sh
@@ -15,12 +15,15 @@ brew install 'db-browser-for-sqlite'
 # productivity
 #brew install 'alfred'
 brew install 'flux'
+brew install raycast
+brew install firefox
 #brew install 'popclip'
 brew install 'hammerspoon'
 brew install 'karabiner-elements'
 #brew install '1password'
 brew install 'simplenote'
 brew install 'the-unarchiver'
+brew install 'openinterminal'
 
 # basics
 brew install 'drawio'
